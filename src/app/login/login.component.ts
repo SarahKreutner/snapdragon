2a516bdcb734f08da808fa11f7bf7964b5faebce
SarahKreutner <skbat@iastate.edu>
2019-11-06 16:19:15 -0600

    2 files modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+ public items: Array<string>;
+  title = 'app';
+  results = '';
+  constructor(private http: HttpClient) {
+    
+  }
 
-  constructor() { }
-
+  public open(event, item) {
+    alert('Open ' + item);
+  }
   ngOnInit() {
+    
+  }
+  myFunc() : void{
+   this.http.get('https://api.github.com/users/seeschweiler').subscribe(data => {
+      console.log(data);
+    });
+  
   }
 
 }
