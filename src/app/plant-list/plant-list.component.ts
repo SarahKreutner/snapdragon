dbcc699ef195106d539cf874754c41ed9e09201b
SarahKreutner <skbat@iastate.edu>
2019-11-06 16:59:54 -0600

    1 file modified

--- src/app/plant-list/plant-list.component.ts
+++ src/app/plant-list/plant-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 import { CartService } from '../cart.service';
 
 import { plants } from '../plants';
