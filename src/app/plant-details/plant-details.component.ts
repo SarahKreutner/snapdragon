53cd8ac26d1e990c5050f20ccfb3fa82b58736e9
SarahKreutner <skbat@iastate.edu>
2019-11-20 16:18:15 -0600

    2 files modified

--- src/app/plant-details/plant-details.component.ts
+++ src/app/plant-details/plant-details.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from "@angular/core";
-import {Router, ActivatedRoute, ParamMap } from "@angular/router";
+import { Router, ActivatedRoute, ParamMap } from "@angular/router";
 import { plants } from "../plants";
-import { CartService } from '../cart.service';
-import { HttpClient } from '@angular/common/http';
-import { switchMap } from 'rxjs/operators';
-import {database, user} from "../constants";
+import { CartService } from "../cart.service";
+import { HttpClient } from "@angular/common/http";
+import { switchMap } from "rxjs/operators";
+import { database, user } from "../constants";
+import { FormBuilder } from "@angular/forms";
 
 @Component({
   selector: "app-plant-details",
@@ -12,22 +13,43 @@ import {database, user} from "../constants";
   styleUrls: ["./plant-details.component.css"]
 })
 export class plantDetailsComponent implements OnInit {
-   plant;
-   id;
-
-  constructor(private http: HttpClient,
-    private route: ActivatedRoute,private cartService: CartService
-  ){}
+  plant;
+  id;
+  plantName;
+  plantDescription;
+  notifications;
+  showInput;
+  addPlantForm;
+  constructor(
+    private http: HttpClient,
+    private route: ActivatedRoute,
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {
+    this.addPlantForm = this.formBuilder.group({
+      plantName: "",
+      plantDescription: "",
+      notifications: false
+    });
+  }
 
   addToCart(plant) {
-    window.alert('Your plant has been added!');
+    window.alert("Your plant has been added!");
     this.cartService.addToCart(plant);
+  }
+  add(plant) {
+    this.showInput = true;
   }
 
   ngOnInit() {
-  this.route.paramMap.subscribe(params => {
-    this.id = params.get('plantId');
-  });
-    this.http.post(database + "/Plants/Database", {plant:{formal_name:this.id,}}).subscribe( data => {console.log(data["data"]); this.plant = data["data"];});
-}
+    this.route.paramMap.subscribe(params => {
+      this.id = params.get("plantId");
+    });
+    this.http
+      .post(database + "/Plants/Database", { plant: { formal_name: this.id } })
+      .subscribe(data => {
+        console.log(data["data"]);
+        this.plant = data["data"];
+      });
+  }
 }
