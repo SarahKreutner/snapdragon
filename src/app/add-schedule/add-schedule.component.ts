af68019bc5cbb1f9c4721b9185a413b29fc2b7fd
SarahKreutner <skbat@iastate.edu>
2019-12-04 17:31:32 -0600

    2 files modified

--- src/app/add-schedule/add-schedule.component.ts
+++ src/app/add-schedule/add-schedule.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from "@angular/forms";
-
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-schedule',
@@ -8,10 +8,25 @@ import { FormBuilder } from "@angular/forms";
   styleUrls: ['./add-schedule.component.css']
 })
 export class AddScheduleComponent implements OnInit {
-
-  constructor() { }
+  scheduleForm;
+  scheduleType;
+  day;
+  frequency;
+  constructor(private http: HttpClient,  private formBuilder: FormBuilder,) {
+    this.scheduleForm = this.formBuilder.group({
+      scheduleType:'',
+      day: '',
+      frequency: ''
+    });
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(): void {
+    this.scheduleType=this.scheduleForm.get('scheduleType').value;
+    this.day=this.scheduleForm.get('day').value;
+    this.frequency=this.scheduleForm.get('frequency').value;
   }
 
 }
