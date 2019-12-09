54700eb8adb014ac63026fd3ae4ecea27df4a128
SarahKreutner <skbat@iastate.edu>
2019-12-09 17:15:04 -0600

    3 files modified

--- src/app/plant-alerts/plant-alerts.component.ts
+++ src/app/plant-alerts/plant-alerts.component.ts
@@ -1,14 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
 import { Output, EventEmitter } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {database, user} from "../constants";
 
 @Component({
   selector: 'app-plant-alerts',
   templateUrl: './plant-alerts.component.html',
   styleUrls: ['./plant-alerts.component.css']
 })
-export class plantAlertsComponent {
+export class plantAlertsComponent implements OnInit{
+  constructor(private http: HttpClient) { }
   @Input() plant;
   @Output() notify = new EventEmitter();
+  plants;
+
+  ngOnInit(){
+    console.log("Creating Notification Subscriber");
+    //console.log(user);
+    setInterval(() => {
+      this.getNotifications(); 
+    }, 1000);
+  }
+  
+  getNotifications() {
+    //console.log("Checking Notifications");
+    this.http.post(database + "/PlantSchedule/getNotifications", {user:{user_id: user,}}).subscribe( data => {
+      console.log(data["data"]); 
+      this.plants = data["data"];
+      // for(let plant_notification of this.plants) {
+      //   if(plant_notification) {
+      //     console.log("hello")
+      //   }
+      // }
+    });
+  }
 
 }
