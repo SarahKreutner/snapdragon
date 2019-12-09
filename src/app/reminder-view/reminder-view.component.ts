5ef9e0c8653fc02e9332806decc759112a68cde0
SarahKreutner <skbat@iastate.edu>
2019-12-09 15:14:42 -0600

    3 files modified

--- src/app/reminder-view/reminder-view.component.ts
+++ src/app/reminder-view/reminder-view.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {database} from "../constants";
 @Component({
   selector: 'app-reminder-view',
   templateUrl: './reminder-view.component.html',
   styleUrls: ['./reminder-view.component.css']
+  
 })
 export class ReminderViewComponent implements OnInit {
-
-  constructor() { }
+  @Input() plant_id;
+  today;
+  closest_water_date;
+  next_water_date;
+  days;
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.today = Date.now;
+    this.http.post(database + "/PlantSchedule/GetNotificationStatus", {plant:{plant_id: this.plant_id}}).subscribe( data => {console.log(data["data"]); this.closest_water_date = data["data"][0]["Next_Notification"]; console.log(this.plant_id, this.today, this.closest_water_date);});
+    
+    
+    
   }
 
 }
