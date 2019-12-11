08444fce93b343fa6bb4f830bf35782011e9231c
SarahKreutner <skbat@iastate.edu>
2019-12-10 20:35:52 -0600

    1 file modified

--- src/app/reminder-view/reminder-view.component.ts
+++ src/app/reminder-view/reminder-view.component.ts
@@ -19,22 +19,8 @@ export class ReminderViewComponent implements OnInit {
       return new Promise(resolve => setTimeout(resolve, milliseconds));
     };
     //  sleep(500).then(() => {
-    this.today = new Date();
-    this.http
-      .post(database + "/PlantSchedule/GetNotificationStatus", {
-        plant: { plant_id: this.plant_id }
-      })
-      .subscribe(data => {
-        console.log(data["data"]);
-        this.closest_water_date = new Date(data["data"][0]["Next_Notification"]);
-        console.log(this.plant_id, this.closest_water_date, this.today);
-        this.days = Math.ceil(
-          (this.closest_water_date.getTime() - this.today.getTime()) /
-            (1000 * 60 * 60 * 24)
-        );
-      });
     // });
-    // this.loadWaterDate();
+    this.loadWaterDate();
   }
   loadWaterDate() {
     this.today = new Date();
